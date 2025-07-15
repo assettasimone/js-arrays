58bb79ac389560ea32d70a949dee04fda6e60217
assettasimone <assettasimone@gmail.com>
2025-07-15 18:20:06 +0200

    add 5 edited 2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -27,11 +27,10 @@ for (let i = 0; i < teachers.length; i++) {
   if (i === teacherSelector - 1) {
     teachers[i] = newTeacher;
   }
-  console.log(teachers[i])
 }
 
 
-
+console.log(teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 let lastTeacher = null;
@@ -41,10 +40,17 @@ lastTeacher = teachers.pop();
 console.log('Ultimo insegnante rimosso ' + lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+let firstTeacher = null;
+
+firstTeacher = teachers.shift();
+
+console.log('primo elemento rimosso dalla lista ' + firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
+teachers.push('Vanessa');
+
+console.log(teachers)
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
