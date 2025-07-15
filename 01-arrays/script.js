085b8e45d58b0b13c5e99b2792bbc28b54b9ffa8
assettasimone <assettasimone@gmail.com>
2025-07-15 18:04:17 +0200

    add 1

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,7 +10,14 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+let fourthTeacher = null;
+const teacherSelector = 4;
+for (let i = 0; i < teachers.length; i++) {
+  if (i === teacherSelector) {
+    fourthTeacher = teachers[i - 1];
+  }
+}
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
